29d8d42dcb06e5a988f8b6274e16797cfa054dbd
Eric <leagues58@gmail.com>
2020-04-24 13:01:44 -0400

    First attempt at a login screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './stillwater_logo.jpg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='logo.png' className="logo" alt="logo" />
+        
+        <div className="header-text">Stillwater Pub Quiz!</div>
+        <div className="subheader-text">(Now Virtual!)</div>
       </header>
+
+      <div className="signup-form">
+        <form onSubmit={()=>{return null}}>
+          <label>
+            Team Name:
+            <input type="text" value={''} onChange={()=>{return null}} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+      </div>
     </div>
   );
 }
