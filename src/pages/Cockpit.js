db3086ad7d1d136921647f140fe39a4771c29d42
Eric <leagues58@gmail.com>
2020-04-25 15:39:17 -0400

    Beginning to build out cockpit page

--- src/pages/Cockpit.js
+++ src/pages/Cockpit.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
+
+const TeamList = () => {
+  return (
+    <div>
+      <h3>Team List</h3>
+      <ul>
+        <li>Team1</li>
+      </ul>
+    </div>
+  );
+};
+
+const QuestionList = () => {
+  return (
+    <div>
+      <h3>Question List</h3>
+      <ul>
+        <li>Question 1</li>
+      </ul>
+    </div>
+  );
+};
 
 const Cockpit = () => {
   return (
     <div>
-      <h3>Watch the game unfold in realtime.</h3>
+      <h1>Cockpit</h1>
+      <TeamList />
+      <QuestionList />
     </div>
   );
 };
