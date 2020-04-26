24029f5219370118d7c569d86033339098c77c48
Eric <leagues58@gmail.com>
2020-04-25 23:30:07 -0400

    Users can now submit answers to the open question

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,11 +1,55 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import getTeam from '../services/GetTeam';
+import firebase from '../Firebase';
+import {TextField, Button} from '@material-ui/core';
+import submitAnswer from '../services/SubmitAnswer';
+
+const Question = ({children}) => {
+  
+  return (
+    <div>
+      <h2>Question:</h2>
+      {children}
+    </div>
+  );
+};
+
+const AnswerArea = ({questionId, teamId}) => {
+  const [answer, setAnswer] = useState('');
+  const answerSubmit = () => {
+    if (answer === '') {
+      const confirmEmpty = alert('Are you sure you want to submit an empty answer?');
+      if (!confirmEmpty) {
+        return false;
+      }
+    }
+    submitAnswer(answer, questionId, teamId);
+  };
+
+  const answerChangeHandler = (event) => {
+    setAnswer(event.target.value);
+  };
+
+  return (
+    <div>
+      <TextField
+        id="outlined-multiline-static"
+        multiline
+        rows={4}
+        variant="outlined"
+        onChange={answerChangeHandler}
+      />
+      <Button variant='contained' onClick={answerSubmit}>submit your answer</Button>
+    </div>
+  );
+};
 
 
 const Play = () => {
   const {id} = useParams();
-  const [teamData, setTeamData] = useState();
+  const [teamData, setTeamData] = useState({});
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     const getTeamInfo = async () => {
@@ -18,6 +62,36 @@ const Play = () => {
     getTeamInfo();
   }, []);
 
+  useEffect(() => {
+    const getTeamInfo = async () => {
+      const doc = await getTeam(id);
+      if (doc) {
+        setTeamData(doc);
+      }
+    };
+
+    getTeamInfo();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribeCallback = firebase.firestore()
+    .collection('questions')
+    .onSnapshot((snapshot) => {
+      const questions = [];
+      snapshot.forEach((doc) => {
+        questions.push({
+          id: doc.id, 
+          question: doc.data().question, 
+          isOpen: doc.data().open,
+          answers: doc.data().answers
+        });
+      });
+      setQuestions(questions);
+    });
+
+    return () => unsubscribeCallback();
+  }, []);
+
 
   return (
     <div>
@@ -25,6 +99,9 @@ const Play = () => {
       <h4>{id}</h4>
       <h4>Welcome team {teamData?.teamName}</h4>
       <h4>{JSON.stringify(teamData)}</h4>
+      <h4>{JSON.stringify(questions)}</h4>
+      <Question>{questions.find(q => q.isOpen)?.question}</Question>
+      <AnswerArea questionId={questions.find(q => q.isOpen)?.id} teamId={id}/>
     </div>
   );
 };
---
+++ src/services/SubmitAnswer.js
@@ -0,0 +1,17 @@
+import firebase from '../Firebase';
+
+const submitAnswer = async (answer, questionId, teamId) => {
+  const answerObj = {answer, teamId}
+  const ref = await firebase.firestore()
+    .collection('questions')
+    .doc(questionId)
+    .update({
+      answers: firebase.firestore.FieldValue.arrayUnion(answerObj)
+    })
+    .catch((err) => {
+      console.warn('Error saving answer to db', err);
+    });
+};
+
+
+export default submitAnswer;
