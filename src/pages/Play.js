229ba0f0acf6968577edc758b1d6150f962e5b65
Eric <leagues58@gmail.com>
2020-04-28 23:32:13 -0400

    Moved Answer comp to its own file, updated to work with new answer coll

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -2,63 +2,10 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import getTeam from '../services/GetTeam';
 import firebase from '../Firebase';
-import {TextField, Button, AppBar, Paper} from '@material-ui/core';
-import submitAnswer from '../services/SubmitAnswer';
+import {AppBar, Paper} from '@material-ui/core';
 import ScoreCard from '../components/ScoreCard';
 import Question from '../components/Question';
-
-
-const AnswerArea = ({question, teamId}) => {
-  const [answer, setAnswer] = useState('');
-  const [answerSubmitted, setAnswerSubmitted] = useState(false);
-  const [currentQuestionId, setCurrentQuestionId] = useState(0);
-  const answerSubmit = () => {
-    if (answer === '') {
-      alert('You gotta put something!');
-      return false;
-    }
-    submitAnswer(answer, question.id, teamId);
-  };
-
-  const answerChangeHandler = (event) => {
-    setAnswer(event.target.value);
-  };
-
-  useEffect(() => {
-    if (question?.answers?.filter(q => q.teamId === teamId).length > 0) {
-      setAnswerSubmitted(true);
-    } else {
-      setAnswerSubmitted(false);
-    }
-
-    if (question?.id !== currentQuestionId) {
-      setAnswer('');
-      setCurrentQuestionId(question?.id);
-    }
-  }, [question]);
-
-
-  if (question) {
-    return (
-      <div style={{display: 'flex',flexDirection:'column', marginTop:'2vh'}}>
-        <TextField
-          id="outlined-multiline-static"
-          multiline
-          rows={4}
-          variant="outlined"
-          onChange={answerChangeHandler}
-          disabled={answerSubmitted}
-          value={answer}
-        />
-        <Button variant='contained' color='primary' onClick={answerSubmit} disabled={answerSubmitted} style={{marginTop:'1vh'}}>submit your answer</Button>
-      </div>
-    );
-  } else {
-    return (
-      <div></div>
-    );
-  }
-};
+import AnswerArea from '../components/AnswerArea';
 
 
 const Play = () => {
@@ -122,15 +69,12 @@ const Play = () => {
 
   return (
     <div style={{display:'flex', flexDirection:'column', alignItems:'center', padding: '20px', backgroundColor:'lightgray', paddingBottom:'10%'}}>
-      {JSON.stringify(questions)}
-      <br/><br/>
-      {JSON.stringify(answers)}
       <AppBar position="static">
         <span style={{padding: '10px', fontSize:'1.2em', fontWeight: 'bold'}}>Stillwater Pub Quiz - {teamData?.teamName}</span>
       </AppBar>
       <Paper elevation={3} style={{display:'flex', flexDirection:'column', padding: '10px', marginTop: '3vh', width:'90%'}}>
         <Question number={questions.find(q => q.isOpen)?.questionNumber}/>
-        <AnswerArea question={questions.find(q => q.isOpen)} teamId={id}/>
+        <AnswerArea question={questions.find(q => q.isOpen)} teamId={id} answers={answers}/>
       </Paper>
       <Paper elevation={3} style={{display:'flex', flexDirection:'column', padding: '10px', marginTop: '3vh', width:'90%'}}>
         <ScoreCard questions={questions} answers={answers} />
